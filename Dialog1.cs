9c707e95b3c190481a0592aab056bbd9d99106cd
lsimonl <54697119+lsimonl@users.noreply.github.com>
2021-01-09 19:29:06 +0800

    Add files via upload

---
+++ Dialog1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog1 : MonoBehaviour
+{
+    public GameObject EnterDialog;
+    public GameObject dropWeapon;
+    private float count = 0;
+ 
+    
+
+    private void Update()
+    {
+        if (GameObject.Find("player").GetComponent<Grab_controller>().Grabbled)
+        {
+            EnterDialog.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            EnterDialog.SetActive(true);
+            dropWeapon.SetActive(true);
+            
+        }
+        count += 1;
+    }
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Player")
+    //    {
+    //        EnterDialog.SetActive(false);
+         
+    //    }
+    //}
+
+    
+}
